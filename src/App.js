ecaae141e1f24a65a4c13bb15d7df1b5dc382069
austin <austin89peterson@gmail.com>
2018-12-12 15:18:58 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
-import Home from './components/Home'
-import People from './components/People'
+// import Home from './components/Home'
+// import People from './components/People'
 import Routing from './Routing'
-
+import Navbar from './components/Navbar'
 class App extends Component {
   render() {
     return (
       <div>
         <center>
+          <Navbar/>
           <Routing/>
         </center>
       </div>
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -8,26 +8,29 @@ class Auth extends Component {
         e.preventDefault()
     }
 
+    state = {
+      email:'',
+      password:''
+    }
+
   render() {
     return (
-      <div classname='name'>
-        <h1>😍Authentication😍</h1>
         <Form inline onSubmit={this.handleSubmit}>
+        <h1>😍Authentication😍</h1>
         <FormGroup>
           <Label for="exampleEmail" hidden>Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+          <Input type="email" name="email" id="exampleEmail" placeholder="Email" onChange={e => this.setState({email: e.target.value})}/>
         </FormGroup>
         {' '}
         <FormGroup>
           <Label for="examplePassword" hidden>Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+          <Input type="password" name="password" id="examplePassword" placeholder="Password" onChange={e => this.setState({password: e.target.value})}/>
         </FormGroup>
         {' '}
         <Button type='submit'>Submit</Button>
       </Form>
-      </div>
     );
-  }}
-
+  }
+}
 
 export default Auth
